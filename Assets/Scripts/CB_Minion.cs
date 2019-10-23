97da312ed835481f32e4a7aa770b635cc0cd714f
Charbel Bouchmouny <charbel_bouchmouny16@hotmail.com>
2019-10-23 17:46:24 +1100

    CreateUnityProject

---
+++ Assets/Scripts/CB_Minion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// this enemy will move towards the turrrent and dies when shot
+
+public class CB_Minion : MonoBehaviour
+{
+
+    public GameObject turrent;
+    public float speed = 5;
+
+
+
+    private void Start()
+    {
+        turrent = GameObject.Find("Turrent");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // look at turrent
+        transform.LookAt(turrent.transform.position);
+
+
+        // move
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+    }
+}
---
+++ Assets/Scripts/CB_turrent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+//SHIOTS WHEN CLICK 
+public class CB_turrent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ROATAE TO MOUSE POISITION 
+        // the infomation of what raycast has hit
+        RaycastHit hit;
+        // create a raycast to point at the mouses position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       if (Physics.Raycast(ray, out hit, 100))
+        {
+            transform.LookAt(new Vector3(hit.point.x, 0, hit.point.z));
+        }
+        
+        // SHHOT ON CLICK
+    }
+}
